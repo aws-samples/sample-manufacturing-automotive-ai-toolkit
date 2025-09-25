54881e72ce3989270965ad6fd19f9b7aceb059f8
Adam Weber <agweber@amazon.com>
2025-09-25 11:26:09 -0700

    Prepping frontend on new clean branch

--- ui/next.config.js
+++ ui/next.config.js
@@ -7,6 +7,10 @@ const nextConfig = {
     config.externals = config.externals || [];
     config.externals.push('sharp');
     return config;
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 3000
   }
 }
 
